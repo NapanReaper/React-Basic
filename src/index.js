eafa9b9438328f7d9357f9727f3755723fa7deab
Do Tran Nhat Duy <napanreaper2506@gmail.com>
2020-11-11 17:05:34 +0700

    second commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
-import ReactDom  from 'react-dom'
+import ReactDom from 'react-dom'
+import $ from 'jquery'
 
-function Greeting(){
-  return <h1>This is my first Component</h1>
+// function Greeting() {
+//   return <h1>Hello World</h1>
+// }
+const Greeting = () => {
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h1', {}, 'Hello World')
+  )
 }
-ReactDom.render(<Greeting/>,document.getElementById('root'))
+ReactDom.render(<Greeting />, document.getElementById('root'))
